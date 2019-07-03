7f26e8423f660990f48e961789131db91353dd7f
Erik Søe Sørensen <erik@humio.com>
2019-07-03 18:14:57 +0200

    Lexer: redo processing to better remember source positions.
    This meant reimplementing source-line loop and label extraction.

--- dull/lexer.py
+++ dull/lexer.py
@@ -5,7 +5,6 @@ REFERENCE_TEXT = "all work and no play makes jack a dull boy"
 #==== Tokens: ========================================================
 class Token:
     def __eq__(self, other):
-        print("DB: Token.eq: %s vs %s" % (self, other))
         return (isinstance(other, self.__class__)
             and self.__dict__ == other.__dict__)
     def __repr__(self):
@@ -14,77 +13,123 @@ class Token:
 class LabelToken(Token):
     def __init__(self, label):
         self.label = label
-#    def __eq__(self, other): return 
 
 #==== Lexer entry point: ==============================================
 def tokenize(src):
-    src = normalize(src)
-    state = LexerState(src)
-    for c in src:
-        handleCharacter(c, state)
+    state = LexerState()
+    lineNo = 0
+    for line in src.splitlines(True):
+        lineNo += 1
+        line = normalizeLine(src)
+        if line=="": continue
+
+        for i in range(0, len(line)):
+            c = line[i]
+            c2 = line[i+1] if i<len(line)-1 else ' '
+            handleCharacter(c, c2, state, lineNo)
     return state.tokens
 
 #==== Source pre-processing (normalization): ==========================
 
-whitespaceTrimmingRe = re.compile("^[ \t]+|[ \t\r]+$", re.MULTILINE)
-commentTrimmingRe = re.compile("^#.*", re.MULTILINE)
-blankLineTrimmingRe = re.compile("^\n+", re.MULTILINE)
-def normalize(src):
-    (src,_) = whitespaceTrimmingRe.subn("", src)
-    (src,_) = commentTrimmingRe.subn("", src)
-    (src,_) = blankLineTrimmingRe.subn("", src)
-    return src
+def normalizeLine(line):
+    line = line.strip()
+    if line.startswith("#"): return ""
+    return line
     
 #==== Lexer proper - typo detector: ==========================
-def handleCharacter(c, state):
-    #print("char: '%c'" % c)
-    if state.refMatchPos == state.pos:
-        state.addToken(LabelToken(labelFrom(state.src[state.pos:])))
-    state.pos += 1
+def handleCharacter(c, c2, state, lineNo):
+    lb = state.labelBuilder
+    labelDone = lb.done
+    refChar = state.referenceChar()
+    if not labelDone:
+        labelDone = lb.addCharacter(c, refChar)
+        if labelDone:
+            state.addToken(LabelToken(lb.closeAndGetLabel()))
+        else:
+            state.advanceReference()
 
-def labelFrom(s):
-    i=0
-    upperSeen = False
-    wordEnd = 0
-    while i<len(s) and i<len(REFERENCE_TEXT):
-        c = s[i]
-        cl = c.lower()
-        if cl != REFERENCE_TEXT[i].lower():
-            break
-        if c != cl:
-            upperSeen = True
-        if c == " ":
-            if not upperSeen:
-                i = wordEnd
-                break
-            else:
-                upperSeen = False
-                wordEnd = i
-        i += 1
-    return s[0:i]
+    if labelDone:
+        # Identify text mutations:
+        identifyMutation(state, c, c2)
+
+def identifyMutation(state, c, c2):
+    refChar = state.referenceChar()
+    # TODO:
+    #print("DEBUG identifyMutation: %s->%s ref=%s" % (c,c2,refChar))
+    # if c == refChar:
+    #     if c2 == refChar and c2 != state.nextReferenceChar():
+    #         # Doubled character.
+    #     else:
+    #         # No change.
+    adv = True
+    if adv: state.advanceReference()
 
 class LexerState:
-    def __init__(self, src):
-        self.src = src          # The program source text
-        self.pos = 0            # Current read position
-        self.lineNo = 1         # Current line number
+    def __init__(self):
         self.refPos = 0         # Position in reference text
-        self.lineStart = 0      # Position of the start of the current line
-        self.refMatchPos = 0    # Position in the source where current ref-matchup starts
         self.tokens = []        # The tokens found so far
+        self.labelBuilder = LabelBuilder()  # The line label
         
-    def setLineStart(self):
-        self.lineStart = self.pos
-
-    def lineNumber(self):
-        return self.lineNo
-    def columnNumber(self):
-        return self.pos - self.lineStart + 1
-
     def currentLine(self):
         endPos = self.src.indexOf("\n", self.lineStart)
         if endPos<0: endPos = len(self.src)
         return self.src[self.lineStart:endPos]
 
+    def resetLabel(self):
+        self.labelBuilder = ""
+
+    def addToLabel(self, c):
+        self.labelBuilder += c
+    
     def addToken(self, token):
         self.tokens.append(token)
+
+    def referenceChar(self):
+        return REFERENCE_TEXT[self.refPos]
+    def nextReferenceChar(self):
+        return REFERENCE_TEXT[self.refPos+1]
+
+    def advanceReference(self):
+        self.refPos += 1
+
+class LabelBuilder:
+    def __init__(self):
+        self.buffer = ""
+        self.lastUppercasePos = -1
+        self.lastSpacePos = -1
+        self.nextLastSpacePos = -1
+        self.done = False
+
+    def addCharacter(self, c, refChar):
+        if not self.done:
+            clow = c.lower()
+            if clow != refChar:
+                self.finalizeLabel()
+            else:
+                pos = len(self.buffer)
+                if c==" ":
+                    self.nextLastSpacePos = self.lastSpacePos
+                    self.lastSpacePos = pos
+                    if self.shouldTrim():
+                        self.finalizeLabel()
+                elif c>="A" and c<="Z":
+                    self.lastUppercasePos = pos
+                if not self.done:
+                    self.buffer += c
+        return self.done
+    
+    def closeAndGetLabel(self):
+        if not self.done:
+            self.finalizeLabel()
+        return self.buffer
+
+    def finalizeLabel(self):
+        self.done = True
+        self.trimToLastIncludedWord()
+
+    def trimToLastIncludedWord(self): # private
+        if self.shouldTrim():
+            self.buffer = self.buffer[0:self.nextLastSpacePos]
+
+    def shouldTrim(self):
+        return self.nextLastSpacePos >= self.lastUppercasePos
